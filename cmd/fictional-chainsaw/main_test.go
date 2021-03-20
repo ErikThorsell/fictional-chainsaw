ecf9d396189a07e4548871309c16f190a75a670c
Erik Thorsell <erik@thorsell.cc>
2021-03-20 15:18:23 +0100

    feat: Rewrite application to a web calculator
    * feat: Rewrite application to a web calculator
    * Add containerization step to workflow

--- cmd/fictional-chainsaw/main_test.go
+++ cmd/fictional-chainsaw/main_test.go
@@ -1,15 +1,79 @@
 package main
 
 import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestSquare(t *testing.T) {
+func TestIndex(t *testing.T) {
 
-	expectedOutput := 4
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	if square(2) != expectedOutput {
-		t.Errorf("Test failed, expected: '%d', got: '%d'", expectedOutput, square(2))
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(index)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v, wanted %v", status, http.StatusOK)
+	}
+
+	expected := "This is a calculator!\n"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned wrong status code: got %v, wanted %v", rr.Body.String(), expected)
+	}
+
+}
+
+func TestSum(t *testing.T) {
+
+	post_req := []byte(`{"operation": "sum", "numbers": [1,2,3,4,5]}`)
+	req, err := http.NewRequest("POST", "/calc", bytes.NewBuffer(post_req))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(calc)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v, wanted %v", status, http.StatusOK)
+	}
+
+	expected := "The sum of [1 2 3 4 5] is 15"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned wrong status code: got %v, wanted %v", rr.Body.String(), expected)
+	}
+
+}
+
+func TestMul(t *testing.T) {
+
+	post_req := []byte(`{"operation": "mul", "numbers": [1,2,3,4,5]}`)
+	req, err := http.NewRequest("POST", "/calc", bytes.NewBuffer(post_req))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(calc)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v, wanted %v", status, http.StatusOK)
+	}
+
+	expected := "The product of [1 2 3 4 5] is 120"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned wrong status code: got %v, wanted %v", rr.Body.String(), expected)
 	}
 
 }
